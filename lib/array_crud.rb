7f62efe37d7c8afdac59f87fab6e1195ac79d6f4
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-28 14:37:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array.push("colson")
+add_element_to_end_of_array.push(4, "colson")
 end
 
 def add_element_to_start_of_array(array, element)
