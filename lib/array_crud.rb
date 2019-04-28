8243edb7e7bfa2cbc64ffdc2e62330331914d8e8
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-28 14:47:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 add_element_to_end_of_array = ["ironman", "antman", "spiderman", "thor"]
 
-add_element_to_end_of_array << "fury"
+add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
